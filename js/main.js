e844f7c86385e66849eba3c74f3d4b0eec6e21e6
Sadriev811 <129506177+Sadriev811@users.noreply.github.com>
2023-06-14 17:26:29 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btnSpan = document.querySelector('.span')
+let menu = document.querySelector('.menu_1')
+
+btnSpan.addEventListener('click', function(){
+    menu.classList.toggle('active1')
+})
